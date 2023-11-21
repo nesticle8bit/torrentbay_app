ff76faee0429f037c88b351ac862cf5b5971c499
Julio Poveda <jjpoveda92@gmail.com>
2023-11-21 16:31:16 -0500

    first commit

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { AppComponent } from '../app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
