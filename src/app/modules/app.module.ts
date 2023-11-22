58f56e13c59dfe1fdfbe7b2ffd43322ff4b88707
Julio Poveda <jjpoveda92@gmail.com>
2023-11-22 08:51:16 -0500

    tailwind installation and wip header

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from '../app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../app.component';
+import { FooterComponent } from '../components/main/footer/footer.component';
+import { HeaderComponent } from '../components/main/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
